496093024abd0220674fb6ae32c36ee3cb4a5b00
Dev Sidhwani <devsidhwani000@gmail.com>
2025-07-24 20:57:00 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,42 +1,9 @@
-// const mongoose = require('mongoose');
-
-// // Defining MongoDB connection URL
-// const mongoURl =  'mongodb+srv://Dev:Dev12345@cluster0.4ety5lf.mongodb.net/hotel';
-
-
-// // Set Up MongoDB Connection
-// mongoose.connect(mongoURl );
-
-
-// // get the default connection
-// // Mongoose maintains a default connection object representing the MongoDB connection
-
-// const db = mongoose.connection;
-
-// // Define Event Listeners for Database Connection
-// db.on('connected', () =>{
-//     console.log('Connected to MongoDB Server')
-// });
-
-// db.on('error', (err) =>{
-//     console.log('MongoDB Connection Error')
-// });
-
-// db.on('disconnected', () =>{
-//     console.log('MongoDB Disconnected')
-// });
-
-// // Export the Database Connection
-// module.exports = db;
-
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURl = 'mongodb+srv://Dev:Dev12345@cluster0.4ety5lf.mongodb.net/hotel?retryWrites=true&w=majority';
-
-mongoose.connect(mongoURl, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-});
+// const mongoURl = 'mongodb+srv://Dev:Dev12345@cluster0.4ety5lf.mongodb.net/hotel?retryWrites=true&w=majority';
+const mongoURL = process.env.MONGODB_URL;
+mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
 
--- server.js
+++ server.js
@@ -60,71 +60,14 @@
 // console.log(filter);
 
 
-// Creating a server
-// const express = require('express');
-// const app = express();
-// const db = require('./db');
-// const Person = require('./models/Person')
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-
-// app.use(express.json());
-
-
-
-// app.get('/emp', (req,res) =>{
-//     var empData = {
-//         name:"alex",
-//         id:123,
-//         city:'mumbai'
-//     }
-//     res.send(empData)
-// })
-
-// app.post('/person', async (req, res) =>{
-//       try{
-//       // Assuming the request body contains the person data
-//       const data = req.body;  
-//       //  create a new person document using the mongoose model
-//       const newPerson = new Person(data);
-
-//       // Save the new person to the database
-//       const response = await newPerson.save();
-//       console.log('Data Saved');
-//       res.status(200).json(response);
-        
-//       }catch(err){
-//           console.log(err);
-//           res.status(500).json({error: 'Internal Server Error'})
-//       }
-// })
-
-// app.listen(3000, () =>{
-//   console.log('listening on port 3000');
-// })  
-
-
-// app.get('/data', (req, res) =>
-// {
-//   res.send("Revolve Solutions Private Limited");
-// })
-
-// app.listen(7000,() =>{
-//   console.log("Listening To Port 7000")
-// });
-
-
-// app.get('/me', (req, res) => {
-//   res.send("Internship");
-// })
-// app.listen(6000, () => {
-//   console.log("Listening to port 6000")
-// })
 
 const express = require('express');
 const app = express();
 const db = require('./db');
 const Person = require('./models/Person');
+require('dotenv').config();
+
+const PORT  = process.env.PORT || 3000
 
 
 app.use(express.json()); // to parse incoming JSON
@@ -134,45 +77,9 @@ app.get('/emp', (req, res) => {
 });
 
 
-
-
-
-
-
-
-
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
-
-// // POST Method to add a Menu item
-// app.post('/menu', async (req, res) =>{
-//   try{
-//     const data = req.body
-//     const newMenu = new Menu(data);
-//     const response = await newMenu.save();
-//     console.log('dta saved');
-//     res.status(200).json(response);
-//   }
-//   catch(err){
-//     console.log(err);
-//     res.status(500).json({error: 'Internal Server Error'})
-//   }
-// })
-
-// // GET method to get the Menu Item
-// app.get('/menu', async (req, res) =>{
-//   try{
-//     const data = await Menu.find();
-//     console.log('data fetched');
-//     res.status(200).json(data);
-//   }
-//   catch(err){
-//     console.log(err);
-//     res.status(500).json({error: 'Internal Seerver Error'})
-//   }
-// })
 
 
 
@@ -184,7 +91,9 @@ app.get("/", (req, res) => {
 app.use('/person', personRoutes);
 app.use('/menu', menuRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log('🚀 Server listening on http://localhost:3000');
 });
 
